7c81dd038fcde012a138e8fa3964cf8e69c05030
khairihayat <khairihayat@gmail.com>
2016-12-11 16:41:25 +0800

    test

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: 'prod',
+  apiUrl: 'http://inventory-khairi.azurewebsites.net/api/',
+  title: 'NihonFood'
+};
---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: 'stage',
+  apiUrl: 'http://inventory-khairi.azurewebsites.net/api/',
+  title: 'PhonePlc'
+};
